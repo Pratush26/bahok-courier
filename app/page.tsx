6555626ee5485e63a90b51ea101eb3a13dd552c8
Pratush <pratushbarua26@gmail.com>
2025-06-04 13:24:51 +0600

    vercel fixing

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <section className="grid grid-cols-1 sm:grid-cols-2 justify-items-center items-center gap-6 w-full min-h-[80vh] px-16">
-        <span className="flex flex-col justify-center items-baseline font-bold fontgenos text-black gap-2">
+        <span className="flex flex-col justify-center items-baseline font-bold fontgenos text-black gap-2 order-2 md:order-1">
           <h2 className="text-4xl">Ship your percel now throgh</h2>
           <h1 className="text-8xl text-purple-800 flex items-baseline flex-col md:gap-4 lg:flex-row">Bahok <p className="text-3xl">courier-service</p></h1>
           <h2 className="text-4xl">to enjoy fast and safe delivery experience</h2>
         </span>
         <Image
-          src="/window.svg"
+          src="/delivery1.png"
           alt="Next.js Logo"
-          width={200}
-          height={200}
-          className="mt-8"
+          width={400}
+          height={400}
+          className="order-1 md:order-2"
         />
       </section>
       <p className="mt-4 text-lg">
